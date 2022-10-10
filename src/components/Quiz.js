51649cfdc932e8bfdd6fb7426dd0a94617e9290f
mickeymaruf <thegoldenpopo@gmail.com>
2022-10-10 23:45:47 +0600

    added animation to quiz card

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 const Quiz = ({ quiz }) => {
     const { id, name, logo, total } = quiz;
     return (
-        <div className='bg-slate-800 rounded-md p-3 hover:shadow-lg hover:shadow-slate-700/75 shadow-blur-md'>
+        <div className='bg-slate-800 rounded-md p-3 hover:scale-105 duration-300 relative'>
             <img className='bg-slate-600 mb-3' src={logo} alt={name} />
             <div className='flex justify-between items-center'>
                 <h5>{name}</h5>
-                <Link to={`/quiz/${id}`} className='bg-indigo-500 py-2 px-6 rounded-lg'>Start Quiz</Link>
+                <Link to={`/quiz/${id}`} className='bg-indigo-500 py-2 px-6 rounded-lg hover:shadow-lg hover:shadow-slate-700'>Start Quiz</Link>
             </div>
+            <div className='bg-green-600 p-2 px-5 rounded-full text-sm absolute top-0 right-0 translate-x-3 -translate-y-3'>Total Quiestions: {total}</div>
         </div>
     );
 };
--- src/components/TakeQuiz.js
+++ src/components/TakeQuiz.js
@@ -4,20 +4,19 @@ import { useLoaderData } from 'react-router-dom';
 const TakeQuiz = () => {
     const quiz = useLoaderData().data;
     const { id, name, questions } = quiz;
-    console.log(quiz);
     return (
         <section className='bg-slate-900 text-white py-20'>
             <div className='w-8/12 mx-auto'>
                 <h3 className='text-2xl font-medium text-center mb-8'>Quiz of {name}</h3>
                 {
-                    questions.map((question, idx) => <Quiz key={id} data={question} idx={idx + 1} />)
+                    questions.map((question, idx) => <Question key={id} data={question} idx={idx + 1} />)
                 }
             </div>
         </section>
     );
 };
 
-const Quiz = ({ data, idx }) => {
+const Question = ({ data, idx }) => {
     const { question, options } = data;
     return (
         <div className="bg-slate-700 p-10 text-center mb-10 rounded-xl">
