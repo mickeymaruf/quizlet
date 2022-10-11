e40a83a543cdbe2500b002ef1939fda6a010fea8
mickeymaruf <thegoldenpopo@gmail.com>
2022-10-11 13:27:47 +0600

    reveling answer based on show icon click

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import { useState } from 'react';
 
 
 let uniqueKey = 1;
 
 const Question = ({ data, serial }) => {
     const { correctAnswer, question, options } = data;
+    const [showAns, setShowAns] = useState(false);
+
     const handleAnswer = (selectedAns) => {
         if (selectedAns === correctAnswer) {
             toast.success("Correct answer!", { autoClose: 1500, theme: "colored" });
@@ -15,17 +18,20 @@ const Question = ({ data, serial }) => {
             toast.error("Wrong answer!", { autoClose: 1500, theme: "colored" });
         }
     }
+    const revealAnswer = () => {
+        setShowAns(!showAns);
+    }
     return (
         <div className="bg-slate-700 p-10 text-center mb-10 rounded-xl relative">
             <div className='w-10/12 mx-auto'><code>Quiz {serial}: {question}</code></div>
             <div className='grid grid-cols-2 gap-3 mt-5'>
                 {
                     options.map(option => (
-                        <code onClick={(e) => handleAnswer(e.target.innerText)} key={data.id + uniqueKey++} className='bg-slate-900 p-5 rounded-lg flex items-center justify-center cursor-pointer hover:bg-sky-600'>{option}</code>
+                        <code onClick={(e) => handleAnswer(e.target.innerText)} key={data.id + uniqueKey++} className={`${showAns ? correctAnswer === option ? 'bg-green-600' : 'bg-red-700' : 'bg-slate-900'} p-5 rounded-lg flex items-center justify-center cursor-pointer hover:bg-sky-600`}>{option}</code>
                     ))
                 }
             </div>
-            <div className='absolute top-5 right-5 cursor-pointer'>
+            <div onClick={revealAnswer} className='absolute top-5 right-5 cursor-pointer'>
                 <EyeIcon className="h-6 w-6 text-indigo-400" />
             </div>
         </div>
