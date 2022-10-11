ab67d8eeb03be558d462a3c05099e23fa4b2cae2
mickeymaruf <thegoldenpopo@gmail.com>
2022-10-11 20:07:45 +0600

    navbar height issue fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         <header className='bg-slate-900 py-5'>
             <nav className='w-10/12 mx-auto flex items-center justify-between'>
                 <Link to="/"><img className='w-40' src={logo} alt="logo" /></Link>
-                <div className={`bg-slate-700 backdrop-blur-sm bg-opacity-80 text-white text-2xl md:text-base font-medium flex gap-5 md:gap-8 flex-col md:flex-row md:bg-inherit w-full absolute md:static right-0 ${toggle ? 'top-0' : '-top-[750px]'} h-screen md:h-fit items-center justify-center md:justify-end px-16 py-8 md:p-0 duration-300 z-10`}>
+                <div className={`bg-slate-700 backdrop-blur-sm bg-opacity-80 text-white text-2xl md:text-base font-medium flex gap-5 md:gap-8 flex-col md:flex-row md:bg-inherit w-full absolute md:static right-0 ${toggle ? 'top-0' : '-top-[1000px]'} h-screen md:h-fit items-center justify-center md:justify-end px-16 py-8 md:p-0 duration-300 z-10`}>
                     <NavLink onClick={() => setToggle(false)} className='hover:text-indigo-400' to="/">Home</NavLink>
                     <NavLink onClick={() => setToggle(false)} className='hover:text-indigo-400' to="/">Topics</NavLink>
                     <NavLink onClick={() => setToggle(false)} className='hover:text-indigo-400' to="/statistics">Statistics</NavLink>
